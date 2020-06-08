0bb842462d86d403ee6074d3fecc83af6f0573f7
Chaekang Lee <leechis7@gmail.com>
2020-06-08 15:40:02 +0900

    props

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,19 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>  
-    <h2>{{ dragon }}</h2>  
-    <button @click="vov">추가</button>
-    <home-component></home-component>
-    <dragon></dragon>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <User></User>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import HomeComponent from "./Home.vue";
+  import User from './components/User.vue'
 
-export default {
-  components: {
-    HomeComponent //HomeComponent: HomeComponent
-  },
-  data: function () {
-    return {
-      title: "Hellow2",
-      dragon: 1
-    }  
-  },
-  methods: {
-    vov: function () {
-      this.dragon++;
-      //console.log(this.dragon);
+  export default {
+    components: {
+      User
     }
   }
-};
 </script>
